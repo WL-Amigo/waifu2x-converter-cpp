03de2392945a1df0aeff1e3440b455b8e7904563
tanakmura <tanakmura@gmail.com>
2015-06-13 20:14:19 +0900

    tabify

--- src/modelHandler_CUDA.cpp
+++ src/modelHandler_CUDA.cpp
@@ -15,8 +15,8 @@ static const char prog[] =
 	;
 
 
-#define CUDA_DECL(name)                    \
-    t##name name;
+#define CUDA_DECL(name)				\
+	t##name name;
 
 FOR_EACH_CUDA_FUNC(CUDA_DECL)
 
@@ -28,79 +28,79 @@ cudalib_init(void)
 #ifdef _WIN32
 	handle = LoadLibrary("nvcuda.dll");
 #else
-        handle = dlopen("libcuda.so.1", RTLD_LAZY);
+	handle = dlopen("libcuda.so.1", RTLD_LAZY);
 
 #define GetProcAddress dlsym
 
 #endif
-        if (!handle) {
-                return -1;
-        }
+	if (!handle) {
+		return -1;
+	}
 
-#define LOAD(name)                              \
-        name = (t##name) GetProcAddress(handle, #name); \
-        if (name == NULL) {                 \
-                return -1;                      \
-        }
+#define LOAD(name)					\
+	name = (t##name) GetProcAddress(handle, #name); \
+	if (name == NULL) {				\
+		return -1;				\
+	}
 
-        FOR_EACH_CUDA_FUNC(LOAD);
+	FOR_EACH_CUDA_FUNC(LOAD);
 
-        return 0;
+	return 0;
 }
 
 
 bool
 initCUDA(ComputeEnv *env)
 {
-    if (cudalib_init() < 0) {
-        return false;
-    }
+	if (cudalib_init() < 0) {
+		return false;
+	}
 
-    CUresult r = cuInit(0);
-    if (r != CUDA_SUCCESS) {
-        return false;
-    }
+	CUresult r = cuInit(0);
+	if (r != CUDA_SUCCESS) {
+		return false;
+	}
 
-    int dev_count;
-    cuDeviceGetCount(&dev_count);
+	int dev_count;
+	cuDeviceGetCount(&dev_count);
 
-    if (dev_count == 0) {
-        return false;
-    }
+	if (dev_count == 0) {
+		return false;
+	}
 
-    CUcontext ctxt;
-    CUdevice dev = 0;
-    CUmodule mod;
+	CUcontext ctxt;
+	CUdevice dev = 0;
+	CUmodule mod;
 
-    r = cuCtxCreate(&ctxt, 0, dev);
-    if (r != CUDA_SUCCESS) {
-        return false;
-    }
+	r = cuCtxCreate(&ctxt, 0, dev);
+	if (r != CUDA_SUCCESS) {
+		return false;
+	}
 
-    r = cuModuleLoadData(&mod, prog);
-    if (r != CUDA_SUCCESS) {
-        cuCtxDestroy(ctxt);
-        return false;
-    }
+	r = cuModuleLoadData(&mod, prog);
+	if (r != CUDA_SUCCESS) {
+		cuCtxDestroy(ctxt);
+		return false;
+	}
 
-    CUfunction filter;
+	CUfunction filter;
 
-    r = cuModuleGetFunction(&filter, mod, "filter");
-    if (r != CUDA_SUCCESS) {
-        cuModuleUnload(mod);
-        cuCtxDestroy(ctxt);
-    }
+	r = cuModuleGetFunction(&filter, mod, "filter");
+	if (r != CUDA_SUCCESS) {
+		cuModuleUnload(mod);
+		cuCtxDestroy(ctxt);
+	}
 
-    char name [1024];
-    cuDeviceGetName(name, sizeof(name), dev);
-    printf("CUDA : %s\n", name);
+	char name [1024];
+	cuDeviceGetName(name, sizeof(name), dev);
+	printf("CUDA : %s\n", name);
 
-    env->num_cuda_dev = 1;
-    env->cuda_dev_list = new CUDADev[1];
-    env->cuda_dev_list[0].dev = dev;
-    env->cuda_dev_list[0].context = ctxt;
-    env->cuda_dev_list[0].mod = mod;
-    env->cuda_dev_list[0].filter = filter;
+	env->num_cuda_dev = 1;
+	env->cuda_dev_list = new CUDADev[1];
+	env->cuda_dev_list[0].dev = dev;
+	env->cuda_dev_list[0].context = ctxt;
+	env->cuda_dev_list[0].mod = mod;
+	env->cuda_dev_list[0].filter = filter;
 }
 
 }
--- src/modelHandler_CUDA.cu
+++ src/modelHandler_CUDA.cu
@@ -1,6 +1,14 @@
 /* -*- mode: c++ -*- */
 
-extern "C" __global__ void filter(float *p)
+extern "C" __global__ void 
+filter(const float * __restrict__ packed_input,
+       int nInputPlanes,
+       float * __restrict__ packed_output,
+       int nOutputPlanes,
+       float * __restrict__ biases,
+       unsigned int hsz,
+       unsigned int wsz,
+       float * __restrict__ weight)
 {
-    
+	unsigned int yi = blockIdx.x;
 }
