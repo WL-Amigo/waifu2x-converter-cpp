e8c77f99eaad56a6deff763778319ff516cd60cc
tanakmura <tanakmura@gmail.com>
2015-06-18 23:22:06 +0900

    added install script

---
+++ w32-apps/install.bat
@@ -0,0 +1,1 @@
+wscript install.js
---
+++ w32-apps/install.js
@@ -0,0 +1,7 @@
+var WshShell = WScript.CreateObject("WScript.Shell");
+strDesktop = WshShell.SpecialFolders("SendTo");
+var oShellLink = WshShell.CreateShortcut(strDesktop + "\\waifu.lnk");
+oShellLink.TargetPath = WshShell.CurrentDirectory + "\\w2xcr.exe";
+oShellLink.WindowStyle = 1;
+oShellLink.Description = "waifu2x";
+oShellLink.Save();
