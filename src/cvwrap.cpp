68b01df34f5aa1b39bde2f016b959960298ebe9a
tanakmura <tanakmura@gmail.com>
2015-10-23 09:25:58 +0900

    added opencv exists check

---
+++ src/cvwrap.cpp
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include "cvwrap.hpp"
+
+W2Mat::~W2Mat()
+{
+    if (data_owner) {
+        free(data);
+    }
+}
+
+W2Mat::W2Mat(int width, int height)
+    :data_owner(true),
+     data_width(width),
+     data_height(height),
+     view_top(0),
+     view_left(0),
+     view_width(width),
+     view_height(height)
+{
+    this->data = (float*)malloc(width * height * sizeof(float));
+}
+
+
+W2Mat
+conv_from_cvmat(cv::Mat &m)
+{
+    int w = m.size().width;
+    int h = m.size().height;
+
+    W2Mat wm(w, h);
+
+    for (int yi=0; yi<h; yi++) {
+        float *in = (float*)m.ptr(yi);
+        float *out = wm.data + yi * w;
+
+        memcpy(out, in, w * sizeof(float));
+    }
+
+    return std::move(wm);
+}
+
+cv::Mat
+conv_to_cvmat(W2Mat &m)
+{
+    int w = m.view_width;
+    int h = m.view_height;
+
+    cv::Mat ret = cv::Mat::zeros(cv::Size(w, h), CV_32FC1);
+
+    for (int yi=0; yi<h; yi++) {
+        float *out = (float*)ret.ptr(yi);
+        float *in = m.data + yi * w;
+
+        memcpy(out, in, w * sizeof(float));
+    }
+
+    return std::move(ret);
+
+}
---
+++ src/cvwrap.hpp
@@ -0,0 +1,63 @@
+#ifndef W2MAT_HPP
+#define W2MAT_HPP
+
+#include <utility>
+
+struct W2Mat {
+    bool data_owner;
+
+    float *data;
+    int data_width;
+    int data_height;
+
+    int view_top;
+    int view_left;
+    int view_width;
+    int view_height;
+
+    W2Mat(int data_witdth, int data_height);
+
+    W2Mat(const W2Mat &) = delete;
+    W2Mat& operator=(const W2Mat&) = delete;
+
+    ~W2Mat();
+
+    static W2Mat copy_view(const W2Mat &rhs);
+    static W2Mat copy_full(const W2Mat &rhs);
+    static W2Mat clip_view(const W2Mat &rhs,
+                           int top, int left, int view_width, int view_height);
+
+    W2Mat & operator = (W2Mat && rhs) {
+        this->data_owner = rhs.data_owner;
+
+        this->data_width = rhs.data_width;
+        this->data_height = rhs.data_height;
+
+        this->view_top = rhs.view_top;
+        this->view_left = rhs.view_left;
+        this->view_width = rhs.view_width;
+        this->view_height = rhs.view_height;
+
+        rhs.data_owner = false;
+        rhs.data = nullptr;
+
+        return *this;
+    }
+
+    W2Mat (W2Mat && rhs) {
+        *this = std::move(rhs);
+    }
+};
+
+#ifdef HAVE_OPENCV
+#include <opencv2/opencv.hpp>
+
+typedef cv::Mat Mat_t;
+typedef cv::Point Point_t;
+
+W2Mat conv_from_cvmat(cv::Mat &m);
+cv::Mat conv_to_cvmat(W2Mat &m);
+
+#endif
+
+#endif
