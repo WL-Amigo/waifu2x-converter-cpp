c28430a35759c3a1039240a27b87a4f38b3c2535
HolyWu <holywu@gmail.com>
2016-01-18 17:50:34 +0800

    Fix memory leak
    The W2Mat's constructor which allocates data buffer is called at every
    iteration, and the consumed memory keeps increasing. By declaring the
    variable inside the loop body, the destructor can be called early at the
    end of each iteration.

--- src/convertRoutine.cpp
+++ src/convertRoutine.cpp
@@ -264,9 +264,6 @@ static bool convertWithModelsBlockSplit(W2XConv *conv,
 		}
 	}
 
-	// start to convert
-	W2Mat processBlockOutput;
-
 	if (blockSize == 0) {
 		blockSize = env->pref_block_size;
 	}
@@ -353,6 +350,9 @@ static bool convertWithModelsBlockSplit(W2XConv *conv,
 		}
 
 		for (unsigned int c = 0; c < splitColumns; c++) {
+			// start to convert
+			W2Mat processBlockOutput;
+
 			int clipStartX = c * clipWidth;
 			int clipEndX = 0;
 
