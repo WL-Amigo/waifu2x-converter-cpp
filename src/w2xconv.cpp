f9cf725039fcd6b6f898f9fafdf4b659de99cecf
Yuki <YukihoAA@users.noreply.github.com>
2019-05-08 09:16:51 +0900

    bug fix with scale_ratio

--- src/w2xconv.cpp
+++ src/w2xconv.cpp
@@ -1592,12 +1592,12 @@ int w2xconv_convert_file(
 	}
 #endif
 	
-	// divide images in to 4^n pieces when width is 6000/12000/18000....
+	// divide images in to 4^n pieces when output width is bigger then 13000....
 	std::vector<cv::Mat> pieces, converted;
 	
 	pieces.push_back(image_src);
 	
-	while(pieces.front().rows * pieces.front().cols > 6000 * 6000)
+	while(pieces.front().rows * pieces.front().cols * scale * scale > 13000 * 13000)
 	{
 		cv::Mat front = pieces.front();
 		pieces.erase(pieces.begin());
