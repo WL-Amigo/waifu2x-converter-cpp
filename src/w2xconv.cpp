04b642c5777127e544da06a595b25e4b94b22c33
Yuki <YukihoAA@gmail.com>
2019-05-09 00:36:06 +0900

    Revert "[TEST] heap 200MB"
    This reverts commit 27dede51161345434d8504938ee4f9b67e5497bc.

--- src/w2xconv.cpp
+++ src/w2xconv.cpp
@@ -1,9 +1,6 @@
 #define W2XCONV_IMPL
 #define _WIN32_WINNT 0x0600
 
-#pragma comment(linker, "/STACK:200000000")
-#pragma comment(linker, "/HEAP:200000000")
-
 #define ENABLE_AVX 1
 
 #include <thread>
