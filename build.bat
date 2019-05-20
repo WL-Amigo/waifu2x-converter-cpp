c078335878effa256201552dee13b386198f36a0
Yuki <YukihoAA@users.noreply.github.com>
2019-05-20 16:47:46 +0900

    Revert "temporary disabled type 3 with alpha support"
    This reverts commit e44984eb039e1fb66b887262c86d85252d4a2171.

--- build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d %~dp0
-cd out
-msbuild waifu2xcpp.sln /p:Configuration=Release /p:Platform=x64 -m
-cd ..
--- src/w2xconv.cpp
+++ src/w2xconv.cpp
@@ -1269,7 +1269,7 @@ void get_png_background_colour(FILE *png_fp, bool *png_rgb, struct w2xconv_rgb_f
 	/* use IMREAD_UNCHANGED
 	 * if png && type == RGBA || depth == 16
 	 */
-	if (type == 6 || type == 4) {
+	if (type == 6 || type == 3 || type == 4) { // type == 3 is indexed color 
 		if (depth == 8 || // RGBA 8bit
 		    depth == 16	  // RGBA 16bit
 			)
@@ -1278,10 +1278,9 @@ void get_png_background_colour(FILE *png_fp, bool *png_rgb, struct w2xconv_rgb_f
 		}
 	} else if (depth == 16) { // RGB 16bit
 		*png_rgb = true;
-	} else if (type == 3){ // indexed color
 	}
 	
-	std::cout << "type:" << type << std::endl;
+	// std::cout << "type:" << type << std::endl;
 
 	if (*png_rgb) {
 		while (1) {
