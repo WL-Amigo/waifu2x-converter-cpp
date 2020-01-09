bd4cc9c20d06f90645461e769fef73a7fc253b5b
DeadSix27 <DeadSix27@users.noreply.github.com>
2020-01-09 15:49:59 +0100

    misc: strip copyright header from sources
    As per previous commit, let's just strip it and rely on LICENSE and only mention that said files are part of this project.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /*
 * The MIT License (MIT)
-* Copyright (c) 2015-2020 amigo(white luckers), tanakamura, DeadSix27, YukihoAA and contributors
+* This file is part of waifu2x-converter-cpp
 * 
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
